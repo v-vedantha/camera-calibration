3d693f9af8e2c52459124b2505889e8990f83fe2
Vedantha Venkatapathy <vedantha@Vedanthas-MacBook-Pro.local>
2022-07-16 20:43:09 -0700

    cleaned up image getting

--- get_images_for_calibration.py
+++ get_images_for_calibration.py
@@ -1,19 +1,20 @@
 # import the pygame module, so you can use it
 import os
-from eyes import FrameProcesser
 import pickle
 import pygame
 import numpy as np
 import cv2
 import pyautogui
  
+import random
 # define a main function
 def main():
-    fp = FrameProcesser()
 
+    clock = pygame.time.Clock()
     cam0 = cv2.VideoCapture(0)
-    #cam1 = cv2.VideoCapture(1)
-    cameras = [cam0]#, cam1]
+    cam1 = cv2.VideoCapture(1)
+    cam2 = cv2.VideoCapture(2)
+    cameras = [cam0, cam1, cam2]
 
     # initialize the pygame module
     images = []
@@ -22,47 +23,50 @@ def main():
         retval, image = cameras[i].read()
         images.append(image)
         cv2.imwrite(f"calibration_image_{i}.jpg", image)
-    breakpoint()
 
     pygame.init()
     # create a surface on screen that has the size of 240 x 180
 
     width, height = pyautogui.size().width, pyautogui.size().height
-    screen = pygame.display.set_mode((width,height))
      
     # define a variable to control the main loop
     running = True
     index = 0     
     # main loop
     while running:
+        screen = pygame.display.set_mode((width,height))
         # event handling, gets all event from the event queue
-        for event in pygame.event.get():
+        x = random.random() * 1440
+        y = random.random() * 900
+        # Draw a circle at x, y
+        pygame.draw.circle(screen, (255, 0, 0), (int(x), int(y)), 15)
+        pygame.draw.circle(screen, (0, 0, 255), (int(x), int(y)), 10)
+        pygame.draw.circle(screen, (255, 255, 255), (int(x), int(y)), 3)
+        # update the screen
+        pygame.display.flip()
 
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                index += 1
-                # Take a picture using both cameras
-                images = []
-                if not os.path.exists(f"calibration_images/{index}"):
-                    os.makedirs(f"calibration_images/{index}")
-                if not os.path.exists(f"mouse_positions/{index}"):
-                    os.makedirs(f"mouse_positions/{index}")
+        clock.tick(.7)
+        index += 1
+        # Take a picture using both cameras
+        images = []
+        if not os.path.exists(f"calibration_images/{index}"):
+            os.makedirs(f"calibration_images/{index}")
+        if not os.path.exists(f"mouse_positions/{index}"):
+            os.makedirs(f"mouse_positions/{index}")
 
-                for i in range(len(cameras)):
+        for i in range(len(cameras)):
 
-                    retval, image = cameras[i].read()
-                    images.append(image)
-                    cv2.imwrite(f"calibration_images/{index}/calibration_image_{i}.jpg", image)
-                    fp.process(image)
-                                
-     
-                # Get the mouse position from pygame
-                (x, y) = pygame.mouse.get_pos()
-                with open(f"mouse_positions/{index}/positions.pkl", "wb") as f:
-                    pickle.dump((x, y), f)
+            retval, image = cameras[i].read()
+            images.append(image)
+            cv2.imwrite(f"calibration_images/{index}/calibration_image_{i}.jpg", image)
 
-                print(x, y)
+        with open(f"mouse_positions/{index}/positions.pkl", "wb") as f:
+            pickle.dump((x, y), f)
+
+        print(x, y)
      
             # only do something if the event is of type QUIT
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 # change the value to False, to exit the main loop
                 running = False
